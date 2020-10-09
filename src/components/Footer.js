e9d563b095802a56ec0456cb416c0f7517207f67
Wanderingtech <heather86lyn@gmail.com>
2020-10-08 19:49:41 -0700

    Header, Footer, AboutMe

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+function Footer(){
+    return(
+        <footer>
+        <div className="card text-center">
+          <div className="card-header" id="footerHeader">
+            Check me out
+          </div>
+          <div className="card-body">
+            <h5 className="card-title"></h5>
+            <a href="https://github.com/Wanderingtech" id="github" className="btn btn-primary" target="_blank">
+              <i className="fab fa-github"></i>
+              Github
+            </a>
+              
+            <a href="https://www.linkedin.com/in/heather-stolz-62176a144/" id="linkedin" className="btn btn-primary" target="_blank">
+            <i className="fab fa-linkedin"></i>
+            Linkedin
+          </a>
+            <a href="https://docs.google.com/document/d/1_iE3bIZA7AjrbeyVCxJNvxxW2X_b3UIHUgBV-Q0rvUc/edit?usp=sharing" id="resume" className="btn btn-primary" target="_blank">
+            <i className="fas fa-file"></i>
+            Resume
+            </a>
+          </div>
+        </div>
+      </footer>
+    )
+}
+export default Footer
---
+++ src/pages/AboutMe.js
@@ -0,0 +1,33 @@
+import React from 'react';
+function AboutMe(){
+    return(
+        <div className="container">
+        <div className="row">
+          <div className="w-75 mx-auto">
+            <div className="card">
+              <img src="public/assets/bioprofile.jpg" className="card-image-top-fluid" alt="Picture Of Heather"></img>
+              <div className="card-body" id="aboutMe">
+                <h5 className="card-title">About Me!</h5>
+                <p className="card-text">I've worked in surgery for 13 years. This is the year I broke out,
+                  learned something entirely new, and found a new passion. I have a deep rooted love of learning.
+                  My determination in persuit of learning can be obsessive.
+                  <br></br>
+                  <br></br>
+                  I took on a coding bootcamp that changed my whole outlook on learning styles, and pushed
+                  me to rely on myself and peers rather than soley concrete study materials.
+                  This helped me prepare for the real world from day one.
+                  <br></br>
+                  <br></br>
+                  My experience in surgery built me into a team player. The importance of everyone
+                  being on the same page and being able to contribute their ideas and concerns is very
+                  important to me.
+                </p>
+                <p className="card-text"><small className="text-muted">Last updated 6/27/20</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+    export default AboutMe
+}
