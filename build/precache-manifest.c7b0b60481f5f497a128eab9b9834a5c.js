e2c3c9d3a2ef845cd7b941571f4117033fe22571
Heather Stolz <heather86lyn@gmail.com>
2020-10-17 15:39:11 -0700

    public 2

---
+++ build/precache-manifest.c7b0b60481f5f497a128eab9b9834a5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105b203f268f5edcadcc4dbd7190f610",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "02e20aeed7fa9338a7a1",
+    "url": "/React-Portfolio/static/css/main.ed7a2a46.chunk.css"
+  },
+  {
+    "revision": "6c385ec770b150cb1898",
+    "url": "/React-Portfolio/static/js/2.b0dfb226.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.b0dfb226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e20aeed7fa9338a7a1",
+    "url": "/React-Portfolio/static/js/main.c9b846f7.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  }
+]);
